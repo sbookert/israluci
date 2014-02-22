d1d3a5d9b773e9b4ad06b1e944b4dbad9c18a9b9
vlad <senditin100@gmail.com>
2014-02-21 16:24:19 -0800

    added project and israel's first exercise

---
+++ zzzIsraLuci/src/com/israluci/ch01fundamentalstructures/Welcome.java
@@ -0,0 +1,36 @@
+package com.israluci.ch01fundamentalstructures;
+
+import java.util.*;
+
+
+/**
+
+ * This program displays a greeting from the authors.
+
+ * @version 1.20 2004-02-28
+
+ * @author Cay Horstmann
+
+ */
+
+public class Welcome
+
+{
+
+   public static void main(String[] args)
+
+   {
+
+	   Scanner s = new Scanner(System.in);
+
+      System.out.println("Hello there!");
+
+      System.out.println("Enter some something come on");
+
+      String name = s.nextLine();
+
+      System.out.println("you entered: " + new StringBuilder(name).reverse());
+
+   }
+
+}

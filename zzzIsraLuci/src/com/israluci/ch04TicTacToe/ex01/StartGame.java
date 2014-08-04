b42e2e36f08614520c4e11ff8bc2d80199d30734
israel <213withani@gmail.com>
2014-08-03 21:44:52 -0700

    Moved Board statements from StartGame to UserInterface.

--- zzzIsraLuci/src/com/israluci/ch04TicTacToe/ex01/StartGame.java
+++ zzzIsraLuci/src/com/israluci/ch04TicTacToe/ex01/StartGame.java
@@ -3,22 +3,10 @@ package com.israluci.ch04TicTacToe.ex01;
 public class StartGame {
 
 	public static void main(String[] args) {
+		UserInterface ui=new UserInterface();
 		
-		Board b = new Board();
-		PieceX px = new PieceX();
-		PieceY py = new PieceY();
-		
-		px.setX(1);
-		px.setY(3);
-		
-		System.out.println(b.addPiece(px));
-		System.out.println(b.render());
-		
-		px.setX(3);
-		px.setY(1);
-		
-		System.out.println(b.addPiece(px));
-		System.out.println(b.render());
+		ui.askWhereToPutPiece();
+		ui.showBoard();
 	}
 
 }
--- zzzIsraLuci/src/com/israluci/ch04TicTacToe/ex01/UserInterface.java
+++ zzzIsraLuci/src/com/israluci/ch04TicTacToe/ex01/UserInterface.java
@@ -3,33 +3,41 @@ package com.israluci.ch04TicTacToe.ex01;
 import java.util.Scanner;
 
 public class UserInterface {
-	/*
-	 * - this class should print to console: Where do you want to place your piece? (example: 'x on 1,0' or 'exit')
-	
-	- returns null if user typed exit
-	
-	 * 
-	 * */
+	int x=0,y=0;
+	String turn="";
+
 	public Piece askWhereToPutPiece(){
 		Scanner s = new Scanner(System.in);
-		System.out.println("Where do you want to place your piece? \\(example: \'x on 1,0\' or \'exit\'\\)");
+		System.out.println("Where do you want to place your piece? example: x on 1,0 or exit");
 		String input = s.nextLine();
+		
+		String[] strNum=input.split(" ");
+		turn=strNum[0];
+		
+		
+		for (String str: strNum)
+			System.out.println(str);
+		
+		String[] loc=strNum[2].split(",");
+		x=Integer.parseInt(loc[0]);
+		y=Integer.parseInt(loc[1]);
+
+		
 		if (input.toLowerCase().equals("exit"))
 			return null;		
 		return null;
 	}
 	
-	/*
-	 * - this class prints the board to the screen for example: 
-
-	[ ][X][ ]
-	
-	[ ][ ][O]
-	
-	[ ][ ][ ]
-	*
-	*/
 	public void showBoard(){
+		Board b = new Board();
+		PieceX px = new PieceX();
+		PieceY py = new PieceY();
 		
+		px.setX(x);
+		px.setY(y);
+		
+		System.out.println(b.addPiece(px));
+		System.out.println(b.render());
+
 	}
 }

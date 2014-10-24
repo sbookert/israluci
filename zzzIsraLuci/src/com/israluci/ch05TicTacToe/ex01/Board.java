364f0a372e7011edcaf96ec18fd083100fe19f78
israel <213withani@gmail.com>
2014-10-23 17:55:42 -0700

    Board class: Renamed method and variables for easier code reading.

--- zzzIsraLuci/src/com/israluci/ch05TicTacToe/ex01/Board.java
+++ zzzIsraLuci/src/com/israluci/ch05TicTacToe/ex01/Board.java
@@ -3,50 +3,50 @@ package com.israluci.ch05TicTacToe.ex01;
 public class Board {
 	private int row=3,col=3,max=row*col;
 	private String[][] board= new String[row][col];
-	private String strBracket=" ";	
-	private PieceX px= new PieceX();
-	private PieceO py= new PieceO();
-	private int locX=0, locY=0;
+	private String boardBracket=" ";	
+	private PieceX pieceX= new PieceX();
+	private PieceO pieceO= new PieceO();
+	private int locationX=0, locationY=0;
 	
 	public Board(){
 		//initialize board to brackets [ ]
 		for (int i=0; i<row;i++)
 			for (int j=0; j<col;j++)
-				board[i][j]=strBracket;
+				board[i][j]=boardBracket;
 	}
 	
 	public String render(){	
-		 String strOut="";
+		 String boardUI="";
 		 
 		// concatenate board to string for display
 		for(int i=0; i<row;i++){
 			if(i!=0)
-				strOut+="\n";
+				boardUI+="\n";
 			for(int j=0; j<col; j++){
-				strOut=strOut+ "["+board[i][j]+"]";
+				boardUI=boardUI+ "["+board[i][j]+"]";
 			}
 		}
-		return strOut;	
+		return boardUI;	
 	}
 
 	/*- should return true if the piece can be placed on a square, then put the piece there
 	  - should return false if there is already a piece there or if the square doesn't exist
-	  - the top left square is has x=0, y=0, the bottom right square is x=2, y=2 */
-	public boolean addPiece(Piece p){
-		locX = p.getX()-1;
-		locY = p.getY()-1;
+	  - the top left square is has x=1, y=1, the bottom right square is x=2, y=2 */
+	public boolean addPiece(Piece userPiece){
+		locationX = userPiece.getX()-1;
+		locationY = userPiece.getY()-1;
 
-	    if (p instanceof PieceX)
-	    	px=(PieceX) p;
+	    if (userPiece instanceof PieceX)
+	    	pieceX = (PieceX)userPiece;
 	    else
-	    	py=(PieceO) p;
+	    	pieceO = (PieceO)userPiece;
 			
-		if ((locX >= row || locX < 0) || (locY >= col || locY < 0)){
+		if ((locationX >= row || locationX < 0) || (locationY >= col || locationY < 0)){
 			return false;
 		}
 		else {
 			// If location is empty then add piece
-			if(board[locX][locY].equalsIgnoreCase(strBracket)){
+			if(board[locationX][locationY].equalsIgnoreCase(boardBracket)){
 				addPieceToBoard();				
 				return true;
 			}else {
@@ -56,15 +56,15 @@ public class Board {
 	}
 
 	private void addPieceToBoard() {
-		if(px.isTurn()){
-			board[locX][locY]="X";
-			px.setP("o");
+		if(pieceX.isTurn()){
+			board[locationX][locationY]="X";
+			pieceX.setPlayer("o");
 			Piece.setCount();
 		}
 		
-		if(py.isTurn()){
-			 board[locX][locY]="O";
-			 py.setP("x");
+		if(pieceO.isTurn()){
+			 board[locationX][locationY]="O";
+			 pieceO.setPlayer("x");
 			 Piece.setCount();
 		 }
 		
